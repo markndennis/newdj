9a46ebfcdf75d0250db7b3681e69ff95d010004c
Mark Dennis <markndennis@hotmail.com>
2014-09-04 22:48:57 +0000

    initial commit

---
+++ .c9/metadata/preview-https:/newdj-c9-markndennis.c9.io
@@ -0,0 +1,1 @@
+{"filter":false,"title":"[P] https://newdj-c9-markndennis.c9.io","undoManager":{"mark":0,"position":-1,"stack":[]},"preview":{"path":"https://newdj-c9-markndennis.c9.io","previewer":"preview.browser"},"timestamp":1409868399699,"hash":"da39a3ee5e6b4b0d3255bfef95601890afd80709"}
---
+++ newdj/wsgi.py
@@ -0,0 +1,14 @@
+"""
+WSGI config for newdj project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
+"""
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "newdj.settings")
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
